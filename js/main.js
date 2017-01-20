83f9b5d2e6baab9de0524526ac614d5dd8b091a0
pr3ssh <pablo.martin@enreda.coop>
2017-01-20 18:46:44 +0100

    Added filters by type

--- js/main.js
+++ js/main.js
@@ -6,8 +6,23 @@ var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png',{
 
 var map = L.map('map', {center: latlng, zoom: 11, layers: [tiles]});
 
-var markers = L.markerClusterGroup();
-var organizacionesLayer = L.geoJson(organizaciones);
+var mcg = L.markerClusterGroup(),
+            grupoCooperativas = L.featureGroup.subGroup(mcg), // use `L.featureGroup.subGroup(parentGroup)` instead of `L.featureGroup()` or `L.layerGroup()`!
+            grupoSociedadesLaborales = L.featureGroup.subGroup(mcg),
+            grupoAsociaciones = L.featureGroup.subGroup(mcg),
+            grupoFundaciones = L.featureGroup.subGroup(mcg),
+            grupoResto = L.featureGroup.subGroup(mcg),
+            control = L.control.layers(null, null, { collapsed: false  });
+
+mcg.addTo(map);
+
+var organizacionesLayer = L.geoJson(
+    organizaciones,
+    {
+        onEachFeature: function() {}
+    }
+);
+
 keys = Object.keys(organizacionesLayer._layers);
 for(k=0;k<keys.length;k++) {
     var el = organizacionesLayer._layers[keys[k]];
@@ -31,11 +46,25 @@ for(k=0;k<keys.length;k++) {
             $('#orgmail').text(e.target.options.mail);
             map.panTo(e.latlng);
         });
-    markers.addLayer(marker);
+    // mcg.addLayer(marker);
+        marker.addTo(elprop.Tipo_Entidad == 'Cooperativa' ? grupoCooperativas : elprop.Tipo_Entidad == 'Sociedad Laboral' ? grupoSociedadesLaborales : elprop.Tipo_Entidad == 'Asociación' ? grupoAsociaciones : elprop.Tipo_Entidad == 'Fundación' ? grupoFundaciones : grupoResto);
 }
 
+control.addOverlay(grupoCooperativas, "Cooperativas");
+control.addOverlay(grupoSociedadesLaborales, "Sociedades laborales");
+control.addOverlay(grupoAsociaciones, "Asociaciones");
+control.addOverlay(grupoFundaciones, "Fundaciones");
+control.addOverlay(grupoResto, "Otras");
+control.addTo(map);
+
+grupoCooperativas.addTo(map);
+grupoSociedadesLaborales.addTo(map);
+grupoAsociaciones.addTo(map);
+grupoFundaciones.addTo(map);
+grupoResto.addTo(map);
+
 map.fitBounds(organizacionesLayer.getBounds());
-map.addLayer(markers);
+map.addLayer(mcg);
 
 $('#close').click(function() {
     $('#content-box').hide();
